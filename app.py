e2f0ba3a2486c4e7d3c6b0cc70a1b91875b2e045
Chi Le <lehuechi04@gmail.com>
2024-03-31 23:13:08 -0400

    Add route to app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+@app.route('/getName/<name>')
+def hello(name):
+    return render_template('index.html', username = str(name))
+
+def getName():
+    return "Tuan"
