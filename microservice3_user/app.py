0860a2004c22c623af1a20e9c8c1183e1f4bd432
Chi Le <lehuechi04@gmail.com>
2024-04-03 02:58:31 -0400

    Add login route to user microservice

--- microservice3_user/app.py
+++ microservice3_user/app.py
@@ -8,8 +8,8 @@ import time
 app = Flask(__name__)
 api = Api(app)
 
-users = dict()
-taken_usernames = set()
+users = dict() # Store usernames and passwords
+taken_usernames = set() # Store existed usernames
 
 @app.route('/signup', methods=['POST'])
 def sign_up():
@@ -21,7 +21,20 @@ def sign_up():
     else:
         current_time = time.time()
         taken_usernames.add(name)
+        users[name] = password
         return jsonify({'session': hex(hash(name + "secret" + str(current_time)))}), 200
+    
+@app.route('/login', methods=['POST'])
+def login():
+    user_info = request.json
+    name = user_info['name']
+    password = user_info['pwd']
+    if name not in taken_usernames or users[name] != password:
+        return jsonify({'error': 'Invalid username or password'}), 401
+    else:
+        current_time = time.time()
+        return jsonify({'session': hex(hash(name + "secret" + str(current_time)))}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
