fb28cbd3ff3cd886c0e633cbec11c0bd1b72145e
savvyng <nguyenvysa0512@gmail.com>
2024-05-05 18:45:40 -0400

    Add recipe.html and cart.html, udpate app.py of pantry and recipe

--- microservice4_recipe/app.py
+++ microservice4_recipe/app.py
@@ -1,75 +1,32 @@
-from flask import Flask, jsonify, render_template, request, redirect
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-@app.route('/status')
-def status():
-    return jsonify({'status': 'OK'}), 200
-# redis = Redis(host='localhost', port=6379)
+# Settings
+PANTRY_SERVICE_URL = 'http://localhost:5000/api/cart_contents'
+SPOONACULAR_API_KEY = 'ffaeea8303ac4006bac35629dc34f9a7'
+SPOONACULAR_BASE_URL = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + SPOONACULAR_API_KEY
 
-# def get_selected_items():
-#     selected_items = []
-#     for item_id in redis.smembers('selected_items'):
-#         item = next((item for item in available_items if item['id'] == int(item_id.decode('utf-8'))), None)
-#         if item:
-#             selected_items.append(item)
-#     return selected_items
+@app.route('/recipe')  # Explicitly allowing only GET requests
+def home():
+    # Fetch the cart contents from the Pantry Microservice
+    cart_response = requests.get(PANTRY_SERVICE_URL)
+    if cart_response.status_code != 200:
+        # Return a simple JSON error instead of rendering an error page
+        return jsonify({'error': 'Failed to fetch cart data'}), 502
 
-# @app.route('/', methods=['GET'])
-# def main_page():
-#     return render_template('recipe.html')
+    cart_items = cart_response.json()
+    ingredients = ','.join(cart_items.keys())
 
-# # Your API key for Spoonacular
-# api_key = 'ffaeea8303ac4006bac35629dc34f9a7'
+    # Fetch recipes from Spoonacular
+    recipes_response = requests.get(f"{SPOONACULAR_BASE_URL}&ingredients={ingredients}&number=5")
+    if recipes_response.status_code != 200:
+        # Return a simple JSON error instead of rendering an error page
+        return jsonify({'error': 'Failed to retrieve recipes'}), 503
 
-# @app.route('/recipes', methods=['GET'])
-# def get_recipes():
-#     # Fetch selected items from Redis
-#     items = [item.decode('utf-8') for item in redis.smembers('selected_items')]
-
-#     ingredients_string = ','.join(items)
-
-#     # Call Spoonacular API to fetch recipes
-#     url = f'https://api.spoonacular.com/recipes/findByIngredients?ingredients={ingredients_string}&number=2&apiKey={api_key}'
-
-#     params = {
-#         'apiKey': api_key,
-#         'number': 5,  # Fetch 5 random recipes
-#         'instructionsRequired': True  # Include instructions in the response
-#     }
-#     response = requests.get(url, params=params)
-
-# @app.route('/random-recipes', methods=['GET'])
-# def get_random_recipes_page():
-#     return render_template('random-recipes.html')
-
-# @app.route('/random-recipes', methods=['GET'])
-# def get_random_recipes():
-#     # Call Spoonacular API to fetch random recipes
-#     url = 'https://api.spoonacular.com/recipes/random'
-#     params = {
-#         'apiKey': api_key,
-#         'number': 5,  # Fetch 5 random recipes
-#         'instructionsRequired': True  # Include instructions in the response
-#     }
-#     response = requests.get(url, params=params)
-
-#     # Return the response from Spoonacular API
-#     if response.status_code == 200:
-#         recipes = response.json()['recipes']
-#         # Extract relevant information for each recipe
-#         formatted_recipes = []
-#         for recipe in recipes:
-#             recipe_info = {
-#                 'title': recipe['title'],
-#                 'ingredients': [{'name': ingredient['name'], 'quantity': ingredient['amount'], 'unit': ingredient['unit']} for ingredient in recipe['extendedIngredients']],
-#                 'steps': recipe['instructions'].split('\n') if 'instructions' in recipe else []
-#             }
-#             formatted_recipes.append(recipe_info)
-#         return jsonify(formatted_recipes)
-#     else:
-#         return jsonify({'error': 'Failed to fetch recipes'}), 500
+    recipes = recipes_response.json()
+    return render_template('recipe.html', recipes=recipes)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5004)
+    app.run(port=5004, debug=True)
